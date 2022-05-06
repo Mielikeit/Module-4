555b37c33866a7fda4dc75ca9143aaaf96cf6c79
SydAgui <belagui555@gmail.com>
2022-05-06 16:58:09 -0400

    added changes

---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Game from '../Pages/Game';
+import Home from '../Pages/Home';
+import Login from '../Pages/Auth/Login';
+import {Routes, Route, Link} from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+        <nav classname='nav'>
+        <Link to='/'>Home</Link>
+        <Link to='/game'>Game</Link> 
+        <Link to='/login'>Login</Link>
+
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Mousetrap !</h1>
+        <button
+          title=' '
+          
+        />
+    </div>
+  )
+}
+
+export default Home
